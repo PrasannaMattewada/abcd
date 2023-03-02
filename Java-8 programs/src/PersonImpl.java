b42c65d16955b50e560614975151c5b3802cd2cd
deven <deven@DESKTOP-4CGQLO8>
2023-03-02 16:56:25 +0900

    myfirstcommit

---
+++ Java-8 programs/src/PersonImpl.java
@@ -0,0 +1,15 @@
+interface Person{
+	public abstract void eat();
+}
+public class PersonImpl {
+	public static void main(String[]args) {
+	Person p1= new Person(){
+		
+		public  void eat() {
+			System.out.println("Rich food");
+		}
+		};
+		p1.eat();
+}
+
+}
